2aae38a4f4156cb954d1e47dc1333892eb01822d
Chana023 <jesalchana23@gmail.com>
2023-04-27 20:18:16 +0200

    Completed text challenges

---
+++ 35_uniq_1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# In this challenge, we practice using the uniq command to eliminate consecutive repetitions of a line when a text file is piped through it.
+
+uniq test_data/uniq_1.txt
