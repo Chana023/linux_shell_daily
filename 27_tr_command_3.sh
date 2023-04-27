a4c7a148ec6e56f3e7f889b8f9972249fc7272bc
Chana023 <jesalchana23@gmail.com>
2023-04-27 15:36:33 +0200

    Challenges up to 30 complete

---
+++ 27_tr_command_3.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# In a given fragment of text, replace all sequences of multiple spaces with just one space.
+
+tr -s ' ' < test_data/tr_command_3.txt
+
---
+++ 28_sort_1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# In this challenge, we practice using the sort command to sort input in text or TSV formats.
+
+# Given a text file, order the lines in lexicographical order.
+
+sort test_data/sort_1.tsv
