3cb5a65e99b5f5e61ad5b7812b8b4c4ba7c128ec
Chana023 <jesalchana23@gmail.com>
2023-06-02 20:24:44 +0200

    Display an element in an array

---
+++ 42_display_a_value_in_array.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#Given a list of countries, each on a new line, your task is to read them into an array and 
+# then display the  element indexed at 3. Note that indexing starts from 0.
+
+declare -a countries=()
+
+while read input
+do
+    if [[ -z "$input" ]]
+then 
+    break
+fi
+
+    countries+=("$input")
+
+done
+
+echo "${countries[3]}"
