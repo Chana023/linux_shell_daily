0e1c7334c07a5e2cd5c97c82b904bc2c6fc30256
Chana023 <jesalchana23@gmail.com>
2023-06-03 11:49:35 +0200

    Did another one

--- 42_display_a_value_in_array.sh
+++ 42_display_a_value_in_array.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Given a list of countries, each on a new line, your task is to read them into an array and 
+# Given a list of countries, each on a new line, your task is to read them into an array and 
 # then display the  element indexed at 3. Note that indexing starts from 0.
 
 declare -a countries=()
